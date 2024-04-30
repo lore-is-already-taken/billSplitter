c78dc504e3cf60a375dc8a8223d3f0a642c60f71
Klithik <santi17461@gmail.com>
2024-04-30 17:30:14 -0400

    working on db connection

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module helloWorld
 
 go 1.22.2
+
+require github.com/lib/pq v1.10.9 // indirect
--- server/main.go
+++ server/main.go
@@ -3,7 +3,29 @@ package main
 import (
 	"fmt"
     "net/http"
+    "database/sql"
+  _ "github.com/lib/pq"
 )
+
+const(
+    host = "mypostgres"
+    port = 5432
+    user = "myuser"
+    password = "mypassword"
+    dbname = "mydatabase"
+)
+
+func connect(){
+    // CONNECTS TO DB
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+    panic(err)
+    }
+    defer db.Close()
+}
 
 func main() {
     mux := http.NewServeMux()
@@ -19,7 +41,15 @@ func main() {
         fmt.Printf("inicia sesion con %v,%v",user,pass)
     })
     
-    if err := http.ListenAndServe("localhost:8080",mux); err != nil{
+    mux.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+        fmt.Fprintln(w,"Hello World")
+    })
+    
+    mux.HandleFunc("/connect",func(w http.ResponseWriter,r *http.Request){
+        connect()
+    })
+    
+    if err := http.ListenAndServe(":9090",mux); err != nil{
         fmt.Println(err.Error())
     }
 
