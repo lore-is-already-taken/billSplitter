430da701abf4febb3b9961143c33bc7b29b01b64
Klithik <santi17461@gmail.com>
2024-04-30 12:48:23 -0400

    added basic web server

--- server/main.go
+++ server/main.go
@@ -12,7 +12,13 @@ func main() {
         variable := r.PathValue("variable")
         fmt.Printf("varaible %v",variable)
     })
-
+    
+    mux.HandleFunc("/login/{user}/{pass}",func(w http.ResponseWriter,r *http.Request){
+        user := r.PathValue("user")
+        pass := r.PathValue("pass")
+        fmt.Printf("inicia sesion con %v,%v",user,pass)
+    })
+    
     if err := http.ListenAndServe("localhost:8080",mux); err != nil{
         fmt.Println(err.Error())
     }
